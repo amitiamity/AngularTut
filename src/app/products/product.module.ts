ebe1cb2bf575b37c10abcfafb52bc0fba9cfcf0c
amitiamity <amitptl.in@gmail.com>
2018-09-19 12:36:02 +0530

    implemented shared module concept

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,17 +1,13 @@
 import {ConvertToSpacesPipe} from '../shared/convert-to-spaces.pipe';
-import {StarComponent} from '../shared/star.component';
 import {ProductDetailComponent} from './product-detail.component';
 import { ProductDetailGuard } from './product-detail.guard';
 import {ProductListComponent} from './product-list.component';
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {FormsModule} from '@angular/forms';
 import {RouterModule} from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
     RouterModule.forChild([
       {path: 'products', component: ProductListComponent},
       {
@@ -21,12 +17,12 @@ import {RouterModule} from '@angular/router';
       },
 
 
-    ])
+    ]),
+    SharedModule
   ],
   declarations: [
     ProductDetailComponent,
     ProductListComponent,
-    StarComponent,
     ConvertToSpacesPipe
   ]
 })
